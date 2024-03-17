5add5965342b8b26fe3929d4a1cf26b21d88ce30
Mohammadreza Amini <161158007+JackVey@users.noreply.github.com>
2024-03-17 21:46:48 +0330

    implemented empty methods

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -1,2 +1,36 @@
 public class Menu {
+    static void displayMainMenu(){
+
+    }
+    static void displayStudentDashboard(){
+
+    }
+    static void displayTeacherDashboard(){
+
+    }
+    static void displayAdminDashboard(){
+
+    }
+    static void displaySingInMenu(){
+
+    }
+    static void displaySingUpMenu(){
+
+    }
+    static void displayCourses(){
+
+    }
+    static void displayTeachers(){
+
+    }
+    static void displayStudents(){
+
+    }
+    static void displayAdmins(){
+
+    }
+    static void displaySearchMenu(){
+
+    }
+    
 }
