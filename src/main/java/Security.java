cd8333697ff3d497da0d7064671ec2f5e2f4d183
Mohammadreza Amini <161158007+JackVey@users.noreply.github.com>
2024-03-18 14:40:21 +0330

    added setUUID and hashPassword methods

--- src/main/java/Security.java
+++ src/main/java/Security.java
@@ -1,2 +1,15 @@
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.UUID;
+
 public class Security {
+    static String hashPassword(String password) throws NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance("SHA3-256");
+        md.update(password.getBytes());
+        byte[] digest = md.digest();
+        return new String(digest);
+    }
+    static UUID setUUID(String name){
+        return UUID.nameUUIDFromBytes(name.getBytes());
+    }
 }
