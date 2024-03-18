82d0c2edb55851a6f5b288e496078b41a66f3933
Mohammadreza Amini <161158007+JackVey@users.noreply.github.com>
2024-03-18 16:23:17 +0330

    added empty methods

--- src/main/java/FileHandle.java
+++ src/main/java/FileHandle.java
@@ -1,2 +1,38 @@
 public class FileHandle {
+    static void writeSingUpData(){
+
+    }
+    static void readSingUpData(){
+
+    }
+    static void writeStudentAccountData(){
+
+    }
+    static void readStudentAccountData(){
+
+    }
+    static void writeTeacherAccountData(){
+
+    }
+    static void readTeacherAccountData(){
+
+    }
+    static void writeAdminAccountData(){
+
+    }
+    static void readAdminAccountData(){
+
+    }
+    static void writeCourseData(){
+
+    }
+    static void readCourseData(){
+
+    }
+    static void writeComment(){
+
+    }
+    static void readComment(){
+
+    }
 }
