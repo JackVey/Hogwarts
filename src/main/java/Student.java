dc1623d0ef10fcc06e17df4b0f0cf31b8c7d6752
Mohammadreza Amini <161158007+JackVey@users.noreply.github.com>
2024-03-18 14:53:17 +0330

    completed methods in Student class

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,2 +1,30 @@
-public class Student {
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.UUID;
+
+public class Student implements AccountManagement{
+    private String name;
+    private String username;
+    private String password;
+    private String house;
+    //TODO -> should hash it in Security class
+    private UUID accountID;
+    //TODO -> should set it in Security class
+    private ArrayList<Course> studentCourse = new ArrayList<>();
+    @Override
+    public boolean validatePassword(String enteredPassword) {
+        if (Security.hashPassword(enteredPassword).equals(password))
+            return true;
+        return false;
+    }
+
+    @Override
+    public void changeUsername(String newUsername) {
+        username = newUsername;
+    }
+
+    @Override
+    public void changePassword(String newPassword) {
+        password = Security.hashPassword(newPassword);
+    }
 }
