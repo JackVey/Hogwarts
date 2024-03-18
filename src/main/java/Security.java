bd2daec71098bbe88f5e674b0bf23e70a20c969c
Mohammadreza Amini <161158007+JackVey@users.noreply.github.com>
2024-03-18 14:48:50 +0330

    added try/catch to hashPassword method

--- src/main/java/Security.java
+++ src/main/java/Security.java
@@ -3,11 +3,15 @@ import java.security.NoSuchAlgorithmException;
 import java.util.UUID;
 
 public class Security {
-    static String hashPassword(String password) throws NoSuchAlgorithmException {
-        MessageDigest md = MessageDigest.getInstance("SHA3-256");
-        md.update(password.getBytes());
-        byte[] digest = md.digest();
-        return new String(digest);
+    static String hashPassword(String password) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA3-256");
+            md.update(password.getBytes());
+            byte[] digest = md.digest();
+            return new String(digest);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
     }
     static UUID setUUID(String name){
         return UUID.nameUUIDFromBytes(name.getBytes());
