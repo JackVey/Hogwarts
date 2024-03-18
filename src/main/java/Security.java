118f063c9f8ecc2f7ed615799d4bb548160a0da6
Mohammadreza Amini <161158007+JackVey@users.noreply.github.com>
2024-03-18 14:53:36 +0330

    silencing error

--- src/main/java/Security.java
+++ src/main/java/Security.java
@@ -5,7 +5,7 @@ import java.util.UUID;
 public class Security {
     static String hashPassword(String password) {
         try {
-            MessageDigest md = MessageDigest.getInstance("SHA3-256");
+            MessageDigest md =  MessageDigest.getInstance("SHA3-256");
             md.update(password.getBytes());
             byte[] digest = md.digest();
             return new String(digest);
