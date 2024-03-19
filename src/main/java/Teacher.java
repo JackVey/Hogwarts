5d740c649760bc1c4ab9f2fe559dbea337278100
Mohammadreza Amini <161158007+JackVey@users.noreply.github.com>
2024-03-20 01:21:25 +0330

    implemented methods and added parameters

--- src/main/java/Teacher.java
+++ src/main/java/Teacher.java
@@ -1,2 +1,37 @@
-public class Teacher {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.UUID;
+
+public class Teacher implements AccountManagement{
+    private String name;
+    private String username;
+    private byte[] password = new byte[32];
+    //TODO -> should hash it in Security class
+    private UUID accountID;
+    //TODO -> should set it in Security class
+    private ArrayList<Course> takenCourse;
+    @Override
+    public boolean validatePassword(String enteredPassword) {
+        return Arrays.equals(password, Security.hashPassword(enteredPassword));
+    }
+
+    @Override
+    public void changeUsername(String newUsername) {
+
+    }
+
+    @Override
+    public void changePassword(String newPassword) {
+
+    }
+
+    @Override
+    public void displayDashboard() {
+
+    }
+
+    @Override
+    public void displayProfile() {
+
+    }
 }
