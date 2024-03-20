5eb05f22940d6544e58637937e2c56ee9c063133
Mohammadreza Amini <161158007+JackVey@users.noreply.github.com>
2024-03-20 23:12:30 +0330

    completed course class

--- src/main/java/Course.java
+++ src/main/java/Course.java
@@ -1,2 +1,31 @@
+import java.util.ArrayList;
+
 public class Course {
+    private String name;
+    private ArrayList<String> enrolledStudents;
+    private ArrayList<String> teachers;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<String> getEnrolledStudents() {
+        return enrolledStudents;
+    }
+
+    public void setEnrolledStudents(ArrayList<String> enrolledStudents) {
+        this.enrolledStudents = enrolledStudents;
+    }
+
+    public ArrayList<String> getTeachers() {
+        return teachers;
+    }
+
+    public void setTeachers(ArrayList<String> teachers) {
+        this.teachers = teachers;
+    }
 }
